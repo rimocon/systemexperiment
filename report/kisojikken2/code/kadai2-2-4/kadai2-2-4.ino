b9468022e68f2b82ad0e67ddddb830e7b784afec
rimocon <yamaguchirikiya@yamaguchirikiya-no-MacBook-Air.local>
2019-04-26 12:42:39 +0900

    add report

--- report/kisojikken2/code/kadai2-2-4/kadai2-2-4.ino
+++ report/kisojikken2/code/kadai2-2-4/kadai2-2-4.ino
@@ -1,22 +1,20 @@
-const int LED_RED_PIN = 3;
-const int LED_YEL_PIN = 13;
-const int SW_PIN = 4;
-int sw1;
+const int LED_RED_PIN = 3;//LED_RED_PINを3に定義
+const int LED_YEL_PIN = 13;//LED_YEL_PINを13に定義
+const int SW_PIN = 4; //SW_PINを4に定義
+int sw1;//スイッチの入力保存用
 void setup() {
-  pinMode(LED_RED_PIN,OUTPUT);
-  pinMode(LED_YEL_PIN,OUTPUT);
-  pinMode(SW_PIN,INPUT);
+  pinMode(LED_RED_PIN,OUTPUT);//LED_RED_PINを出力に設定
+  pinMode(LED_YEL_PIN,OUTPUT);//LED_YEL_PINを出力に設定
+  pinMode(SW_PIN,INPUT);//SW_PINを入力に設定
 }
-
-// the loop function runs over and over again forever
 void loop() {
-  sw1=digitalRead(SW_PIN);
-  if(sw1==LOW){
-    digitalWrite(LED_RED_PIN,HIGH);
-    digitalWrite(LED_YEL_PIN,LOW);
+  sw1=digitalRead(SW_PIN);//sw1にSW_PINの値を保存
+  if(sw1==LOW){//スイッチがONなら
+    digitalWrite(LED_RED_PIN,HIGH);//赤をHIGH
+    digitalWrite(LED_YEL_PIN,LOW);//黄色をLOW
   }
   else{
-    digitalWrite(LED_RED_PIN,LOW);
-    digitalWrite(LED_YEL_PIN,HIGH);
+    digitalWrite(LED_RED_PIN,LOW);//赤をLOW
+    digitalWrite(LED_YEL_PIN,HIGH);//黄色HIGH
   }
 }
